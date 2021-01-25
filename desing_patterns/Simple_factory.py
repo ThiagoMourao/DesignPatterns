b8bcd9b71674f31c29013afe3bf80ff1ce28e842
thiago_mourao <thiago.mourao.ifmg@gmail.com>
2021-01-24 22:24:19 -0300

    SimpleFactory

---
+++ desing_patterns/Simple_factory.py
@@ -0,0 +1,30 @@
+# UBER
+from abc import ABC, abstractmethod
+
+
+class Veiculo(ABC):  # Product
+    @abstractmethod
+    def buscar_cliente(self) -> None: pass
+
+class CarroLuxo(Veiculo):  # ConcreteProduct1
+    def buscar_cliente(self) -> None:
+        print('Carro de luxo está buscando clientes...')
+
+class CarroPopular(Veiculo):  # ConcreteProduct2
+    def buscar_cliente(self) -> None:
+        print('Carro popular está buscando clientes...')
+
+class VeiculoFactory:  # Factory
+    def __init__(self, tipo):
+        self.carro = self.get_carro(tipo)
+
+    @staticmethod
+    def get_carro(tipo: str) -> Veiculo:
+        if tipo == 'luxo':
+            return CarroLuxo()
+        if tipo == 'popular':
+            return CarroPopular()
+        assert 0, 'Veículo não existe'
+
+    def buscar_cliente(self):
+        self.carro.buscar_cliente()
---
+++ main.py
@@ -0,0 +1,9 @@
+from random import choice
+from desing_patterns.Simple_factory import VeiculoFactory
+
+
+carros_disponiveis = ['luxo', 'popular']
+
+for i in range(10):
+    carro = VeiculoFactory(choice(carros_disponiveis))
+    carro.buscar_cliente()
