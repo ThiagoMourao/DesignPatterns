deb3d962e1b907e2f1e6da7faf884de9b200c00a
thiago_mourao <thiago.mourao.ifmg@gmail.com>
2021-01-24 22:33:56 -0300

    SimpleFactory

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from desing_patterns.Simple_factory import VeiculoFactory
 
 carros_disponiveis = ['luxo', 'popular']
 
-for i in range(10):
+for i in range(10):  
     carro = VeiculoFactory(choice(carros_disponiveis))
     carro.buscar_cliente()
